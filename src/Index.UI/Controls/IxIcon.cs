dcc20541d827767f615cb20101e7930e9c8022e4
Haus <reid@wilden.haus>
2022-11-20 19:51:54 -0500

    Add IxTextBox/IxIcon controls and styling

---
+++ src/Index.UI/Controls/IxIcon.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Index.UI.Controls
+{
+
+  public class IxIcon : ContentControl
+  {
+
+    #region Properties
+
+    public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+      nameof( Icon ),
+      typeof( object ),
+      typeof( IxIcon ),
+      new PropertyMetadata( null, OnIconChanged ) );
+
+    public object Icon
+    {
+      get => ( object ) GetValue( IconProperty );
+      set => SetValue( IconProperty, value );
+    }
+
+    public static readonly DependencyProperty SegoeIconProperty = DependencyProperty.Register(
+      nameof( SegoeIcon ),
+      typeof( SegoeIcon ),
+      typeof( IxIcon ) );
+
+    public SegoeIcon SegoeIcon
+    {
+      get => ( SegoeIcon ) GetValue( SegoeIconProperty );
+      set
+      {
+        SetValue( SegoeIconProperty, value );
+        SetValue( PathProperty, null );
+      }
+    }
+
+    public static readonly DependencyProperty PathProperty = DependencyProperty.Register(
+      nameof( Path ),
+      typeof( Path ),
+      typeof( IxIcon ) );
+
+    public Path Path
+    {
+      get => ( Path ) GetValue( PathProperty );
+      set
+      {
+        SetValue( PathProperty, value );
+        SetValue( SegoeIconProperty, SegoeIcon.None );
+      }
+    }
+
+    #endregion
+
+    #region Constructor
+
+    static IxIcon()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(
+        typeof( IxIcon ),
+        new FrameworkPropertyMetadata( typeof( IxIcon ) ) );
+    }
+
+    #endregion
+
+    #region Event Handlers
+
+    private static void OnIconChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
+    {
+      var icon = ( IxIcon ) d;
+
+      if ( e.NewValue is SegoeIcon segoeIcon )
+        icon.SegoeIcon = segoeIcon;
+      else if ( e.NewValue is Path path )
+        icon.Path = path;
+    }
+
+    #endregion
+
+  }
+
+}
---
+++ src/Index.UI/Controls/IxTextBox.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Index.UI.Controls
+{
+
+  public class IxTextBox : TextBox
+  {
+
+    #region Properties
+
+    public static readonly DependencyProperty HasTextProperty = DependencyProperty.Register(
+      nameof( HasText ),
+      typeof( bool ),
+      typeof( IxTextBox ) );
+
+    public bool HasText
+    {
+      get => ( bool ) GetValue( HasTextProperty );
+      set => SetValue( HasTextProperty, value );
+    }
+
+    public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+      nameof( Icon ),
+      typeof( object ),
+      typeof( IxTextBox ) );
+
+    public object Icon
+    {
+      get => ( object ) GetValue( IconProperty );
+      set => SetValue( IconProperty, value );
+    }
+
+    public static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register(
+      nameof( PlaceholderText ),
+      typeof( string ),
+      typeof( IxTextBox ) );
+
+    public string PlaceholderText
+    {
+      get => ( string ) GetValue( PlaceholderTextProperty );
+      set => SetValue( PlaceholderTextProperty, value );
+    }
+
+    #endregion
+
+    #region Constructor
+
+    static IxTextBox()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(
+        typeof( IxTextBox ),
+        new FrameworkPropertyMetadata( typeof( IxTextBox ) ) );
+    }
+
+    #endregion
+
+    #region Overrides
+
+    protected override void OnTextChanged( TextChangedEventArgs e )
+    {
+      base.OnTextChanged( e );
+      HasText = Text.Length != 0;
+    }
+
+    #endregion
+
+  }
+
+}
