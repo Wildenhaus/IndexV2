ff20e0e554e6f78488f5b015a82aa361729e17da
Haus <reid@wilden.haus>
2022-11-22 00:32:47 -0500

    Refactor ViewModel base classes

---
+++ src/Index.UI/ViewModels/TabViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Index.UI.ViewModels
+{
+
+  public abstract class TabViewModelBase : ViewModelBase
+  {
+
+    #region Properties
+
+    public virtual string TabName { get; protected set; }
+
+    #endregion
+
+  }
+
+}
---
+++ src/Index.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using PropertyChanged;
+
+namespace Index.UI.ViewModels
+{
+
+  [AddINotifyPropertyChangedInterface]
+  public abstract class ViewModelBase : INotifyPropertyChanged
+  {
+
+    #region Events
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    #endregion
+
+  }
+
+}
