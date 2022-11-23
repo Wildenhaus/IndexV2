17e6421deeee2ba39879d3f03752d5838d7eee93
Haus <reid@wilden.haus>
2022-11-23 13:07:10 -0500

    Add UnhandledExceptionDialog

---
+++ src/Index.App/Prism/DialogServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Index.App.Views;
+using Prism.Services.Dialogs;
+
+namespace Index.App.Prism
+{
+
+  public static class DialogServiceExtensions
+  {
+
+    public static void ShowUnhandledExceptionDialog( this IDialogService dialogService, Exception exception, Action<IDialogResult> callback = null )
+    {
+      var parameters = new DialogParameters();
+      parameters.Add( nameof( Exception ), exception );
+
+      dialogService.ShowDialog( nameof( UnhandledExceptionDialog ), parameters, callback );
+    }
+
+  }
+
+}
---
+++ src/Index.App/Views/UnhandledExceptionDialog.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using Index.UI.Windows;
+
+namespace Index.App.Views
+{
+
+  public partial class UnhandledExceptionDialog : UserControl
+  {
+
+    public UnhandledExceptionDialog()
+    {
+
+      InitializeComponent();
+
+    }
+
+  }
+
+}
