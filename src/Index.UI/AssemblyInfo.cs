6a3601e03c37d3be3c5697f76a59f8fc2a4f4b16
Haus <reid@wilden.haus>
2022-11-23 09:45:08 -0500

    Organize UI namespaces into url

--- src/Index.UI/AssemblyInfo.cs
+++ src/Index.UI/AssemblyInfo.cs
@@ -1,10 +1,14 @@
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
 )]
+
+[assembly: XmlnsDefinition( "http://index/ui", nameof( Index.UI ) )]
+[assembly: XmlnsDefinition( "http://index/ui", nameof( Index.UI.Controls ) )]
+[assembly: XmlnsDefinition( "http://index/ui", nameof( Index.UI.Converters ) )]
+[assembly: XmlnsDefinition( "http://index/ui", nameof( Index.UI.Extensions ) )]
+[assembly: XmlnsDefinition( "http://index/ui", nameof( Index.UI.Services ) )]
+[assembly: XmlnsDefinition( "http://index/ui", nameof( Index.UI.Windows ) )]
