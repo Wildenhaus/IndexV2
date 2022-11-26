5784af95c64c99d667a0b9a9f90f23a438f4ed11
Haus <reid@wilden.haus>
2022-11-26 10:04:02 -0500

    Implement IDisposable for ViewModelBase

--- src/Index.UI/ViewModels/ViewModelBase.cs
+++ src/Index.UI/ViewModels/ViewModelBase.cs
@@ -1,16 +1,58 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using PropertyChanged;
 
 namespace Index.UI.ViewModels
 {
 
   [AddINotifyPropertyChangedInterface]
-  public abstract class ViewModelBase : INotifyPropertyChanged
+  public abstract class ViewModelBase : IDisposable, INotifyPropertyChanged
   {
 
     #region Events
 
     public event PropertyChangedEventHandler? PropertyChanged;
+
+    #endregion
+
+    #region Data Members
+
+    private bool _isDisposed;
+
+    #endregion
+
+    #region Constructor
+
+    protected ViewModelBase()
+    {
+    }
+
+    ~ViewModelBase()
+    {
+      Dispose( false );
+    }
+
+    #endregion
+
+    #region IDisposable Methods
+
+    public void Dispose()
+      => Dispose( true );
+
+    private void Dispose( bool disposing )
+    {
+      if ( _isDisposed )
+        return;
+
+      if ( disposing )
+        OnDisposing();
+
+      _isDisposed = true;
+    }
+
+    protected virtual void OnDisposing()
+    {
+    }
 
     #endregion
 
