298a40f430d07671970e598fe654296f24166ffe
Haus <reid@wilden.haus>
2022-12-09 08:02:06 -0500

    CEA: Add scene lights

--- src/Profiles/Index.Profiles.HaloCEA/Jobs/LoadSceneJob.cs
+++ src/Profiles/Index.Profiles.HaloCEA/Jobs/LoadSceneJob.cs
@@ -69,6 +69,7 @@ namespace Index.Profiles.HaloCEA.Jobs
       await base.CreateScene( context );
 
       await AddProps();
+      AddLights();
     }
 
     #endregion
@@ -175,6 +176,56 @@ namespace Index.Profiles.HaloCEA.Jobs
 
       foreach ( var child in originalNode.EnumerateChildren() )
         AddPropNode( child, newNode, meshLookup );
+    }
+
+    private void AddLights()
+    {
+      SetStatus( "Loading Lights" );
+      SetCompletedUnits( 0 );
+      SetTotalUnits( Scene.Lights.Count );
+
+      foreach ( var sceneLight in Scene.Lights )
+      {
+        var lightName = sceneLight.Name;
+
+        var light = new Light() { Name = lightName };
+        var lightNode = new Node( lightName, Context.RootNode );
+        Context.RootNode.Children.Add( lightNode );
+
+        var matrix = sceneLight.Matrix.ToAssimp();
+        matrix.Transpose();
+        lightNode.Transform = matrix;
+
+        var pos = sceneLight.Matrix;
+        light.Position = new Vector3D( pos.M41, pos.M42, pos.M43 );
+
+        switch ( sceneLight.LightInfo.LightType )
+        {
+          case 0:
+            light.LightType = LightSourceType.Point;
+            break;
+          case 1:
+            light.LightType = LightSourceType.Spot;
+            break;
+          case 4:
+            light.LightType = LightSourceType.Directional;
+            break;
+        }
+
+        light.ColorDiffuse = new Color3D(
+          sceneLight.Color.X,
+          sceneLight.Color.Y,
+          sceneLight.Color.Z );
+        light.ColorSpecular = light.ColorDiffuse;
+        light.ColorAmbient = light.ColorDiffuse;
+
+        light.AngleOuterCone = sceneLight.Data_0285.Unk_00;
+        light.AngleInnerCone = sceneLight.Data_0285.Unk_01;
+        light.Up = new Vector3D( 0, 0, -1 );
+
+        Context.Scene.Lights.Add( light );
+        IncreaseCompletedUnits( 1 );
+      }
     }
 
     #endregion
