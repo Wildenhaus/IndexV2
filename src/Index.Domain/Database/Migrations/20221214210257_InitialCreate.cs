5690442a0a87d0754cbf10d9f637f530de838ed4
Haus <reid@wilden.haus>
2022-12-14 16:27:08 -0500

    Add initial db migrations

---
+++ src/Index.Domain/Database/Migrations/20221214210257_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Index.Domain.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GamePath",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GameId = table.Column<string>(type: "TEXT", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GamePath", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SavedSettings",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GameId = table.Column<string>(type: "TEXT", nullable: false),
+                    Key = table.Column<string>(type: "TEXT", nullable: false),
+                    Data = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedSettings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedSettings_GameId_Key",
+                table: "SavedSettings",
+                columns: new[] { "GameId", "Key" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GamePath");
+
+            migrationBuilder.DropTable(
+                name: "SavedSettings");
+        }
+    }
+}
