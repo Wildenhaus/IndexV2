dd56ed9a90c54e3fe57a31aaf0f117fb4ebadb72
Haus <reid@wilden.haus>
2022-12-19 17:50:36 -0500

    Squashed 'external/LibSaber/' changes from 6e56a9b..c9a955d
    c9a955d H2A: Add HasSkinningData helper property to Vertex
    1c03620 H2A: Make vertex bone indices nullable
    
    git-subtree-dir: external/LibSaber
    git-subtree-split: c9a955dda232acfcef3bba6a5de9749f3f6b8747

--- src/LibSaber.Halo2A/Structures/Geometry/Vertex.cs
+++ src/LibSaber.Halo2A/Structures/Geometry/Vertex.cs
@@ -9,15 +9,20 @@ namespace LibSaber.Halo2A.Structures.Geometry
     public Vector4 Position;
     public Vector4? Normal;
 
-    public byte Index1 { get; set; }
-    public byte Index2 { get; set; }
-    public byte Index3 { get; set; }
-    public byte Index4 { get; set; }
+    public byte? Index1 { get; set; }
+    public byte? Index2 { get; set; }
+    public byte? Index3 { get; set; }
+    public byte? Index4 { get; set; }
 
     public float? Weight1 { get; set; }
     public float? Weight2 { get; set; }
     public float? Weight3 { get; set; }
     public float? Weight4 { get; set; }
+
+    public bool HasSkinningData
+    {
+      get => Index1.HasValue;
+    }
 
   }
 
