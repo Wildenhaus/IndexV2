531cf42dcd6ddd9be86aafdaa3ad90f8bb7000d7
Haus <reid@wilden.haus>
2022-12-19 17:50:20 -0500

    H2A: Add MeshBuilder and SceneContext

---
+++ src/Profiles/Index.Profiles.Halo2A/Meshes/SceneContext.cs
@@ -0,0 +1,64 @@
+﻿using Assimp;
+using HelixToolkit.SharpDX.Core;
+using LibSaber.Halo2A.IO;
+using LibSaber.Halo2A.Structures;
+using LibSaber.IO;
+
+namespace Index.Profiles.Halo2A.Meshes
+{
+
+  internal class SceneContext
+  {
+
+    #region Properties
+
+    public Scene Scene { get; }
+    public Node RootNode => Scene.RootNode;
+
+    public H2AStream Stream { get; }
+    public NativeReader Reader { get; }
+    public GeometryGraph GeometryGraph { get; }
+
+    public Dictionary<short, Bone> Bones { get; }
+    public Dictionary<short, Node> Nodes { get; }
+    public Dictionary<string, Node> NodeNames { get; }
+    public Dictionary<string, int> MaterialIndices { get; }
+    public Dictionary<short, MeshBuilder> SkinCompounds { get; }
+    public Dictionary<short, short> LodIndices { get; }
+
+    #endregion
+
+    #region Constructor
+
+    public SceneContext( GeometryGraph graph, H2AStream stream )
+    {
+      Scene = new Scene();
+
+      Stream = stream;
+      Reader = new NativeReader( stream, Endianness.LittleEndian );
+      GeometryGraph = graph;
+
+      Bones = new Dictionary<short, Bone>();
+      Nodes = new Dictionary<short, Node>();
+      NodeNames = new Dictionary<string, Node>();
+      MaterialIndices = new Dictionary<string, int>();
+      SkinCompounds = new Dictionary<short, MeshBuilder>();
+      LodIndices = new Dictionary<short, short>();
+
+      Scene.Materials.Add( new Material() { Name = "DefaultMaterial" } );
+    }
+
+    public void AddLodDefinitions( IList<LodDefinition> lodDefinitions )
+    {
+      if ( lodDefinitions is null )
+        return;
+
+      foreach ( var lodDefinition in lodDefinitions )
+        LodIndices.Add( lodDefinition.ObjectId, lodDefinition.Index );
+    }
+
+    #endregion
+
+  }
+
+}
