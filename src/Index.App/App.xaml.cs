d3c906979a51a06081838d71773501da6f773f19
Haus <reid@wilden.haus>
2024-09-25 11:45:51 -0500

    #11 - Force en-US Culture as default on startup to fix numeric parsing issues

--- src/Index.App/App.xaml.cs
+++ src/Index.App/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
 using DryIoc;
 
 namespace Index.App
@@ -17,7 +19,27 @@ namespace Index.App
 
     private void OnApplicationStartup( object sender, StartupEventArgs e )
     {
+      SetApplicationCulture();
       new Startup().Run();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void SetApplicationCulture()
+    {
+      // Issue #11: European users may have their system/runtime default to commas
+      // for denoting the decimal separator. Force set the culture to en-US so
+      // that things correctly parse.
+
+      var culture = new CultureInfo( "en-US" );
+
+      Thread.CurrentThread.CurrentCulture = culture;
+      Thread.CurrentThread.CurrentUICulture = culture;
+
+      CultureInfo.DefaultThreadCurrentCulture = culture;
+      CultureInfo.DefaultThreadCurrentUICulture = culture;
     }
 
     #endregion
